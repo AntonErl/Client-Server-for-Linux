ee73c61db97ef40622061074688d6164e75b2616
AntonErl <53444077+AntonErl@users.noreply.github.com>
2019-11-07 08:56:27 +0300

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -13,99 +13,27 @@
 #include <cstring>
 #include <iostream>
 
-std::string exec(const char *cmd)
-{
-    char buffer[1024];
-    std::string result = "";
-    FILE *pipe = popen(cmd, "r");
-    if (!pipe)
-        throw std::runtime_error("popen() failed!");
-    try
-    {
-        while (fgets(buffer, sizeof buffer, pipe) != NULL)
-        {
-            result += buffer;
-        }
-    }
-    catch (...)
-    {
-        pclose(pipe);
-        throw;
-    }
-    pclose(pipe);
-    return result;
-}
-
-char *resize(const char *str, unsigned size, unsigned new_size)
-{
-    char *new_str = new char[new_size];
-    int len = 0;
-    if (size < new_size)
-    {
-        len = size;
-    }
-    else
-    {
-        len = new_size;
-    }
-    for (int i = 0; i < len; i++)
-    {
-        new_str[i] = str[i];
-    }
-    delete[] str;
-    return new_str;
-}
-
-char *getline() // Чтение строки с консоли
-{
-    int ChSize = 40;
-    int ChLen = ChSize - 1;
-    char *buf = resize(0, 0, ChSize);
-    char *str = buf;
-    char const *end = str + ChLen;
-    size_t len = ChLen;
-    char ch = std::getc(stdin);
-    while (ch != EOF && ch != '\n')
-    {
-        *str++ = ch;
-        if (str == end)
-        {
-            buf = resize(buf, len, len + ChSize);
-            str = buf + len;
-            end = str + ChLen;
-            len += ChLen;
-        }
-        ch = std::getc(stdin);
-    }
-    *str = 0;
-    return buf;
-}
-
-unsigned message_len(const char *str)
-{
-    unsigned i = 0;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    return i;
-}
-
 int main(int argc, const char **argv)
 {
+    struct sockaddr_in addr;                    // структура с адресом
+    int sock = socket(AF_INET, SOCK_STREAM, 0); // создание TCP-сокета
+    if (sock < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
     std::string commands = "start";
     while (commands != "exit")
     {
-        char *message = getline();                //Передаваемы данные
-        unsigned mess_len = message_len(message); // размер данных (передаваемого сообщения)
-        commands = message;
+        std::getline(std::cin, commands);      //Читаем данные из стандарного потока ввода в commands
+        unsigned mess_len = commands.length(); // размер данных (передаваемого сообщения)
+
         if (commands != "exit")
         {
 
             char buf[1024];
 
-            struct sockaddr_in addr;                    // структура с адресом
-            int sock = socket(AF_INET, SOCK_STREAM, 0); // создание TCP-сокета
+            sock = socket(AF_INET, SOCK_STREAM, 0); // создание TCP-сокета
             if (sock < 0)
             {
                 perror("socket");
@@ -123,18 +51,12 @@ int main(int argc, const char **argv)
                 exit(2);
             }
 
-            send(sock, message, mess_len + 1, 0);
-            //recv(sock, buf, mess_len + 1, 0);
-            recv(sock, buf, 1024, 0);
+            send(sock, commands.c_str(), mess_len + 1, 0);
+            recv(sock, buf, sizeof buf, 0);
             std::cout << buf << std::endl;
-            close(sock);
-
-            /*std::string result_command = exec(message);
-            std::cout << "result command" << std::endl;
-            std::cout << result_command << std::endl;*/
         }
-        delete message;
     }
     std::cout << "close client" << std::endl;
+    close(sock);
     return 0;
 }
