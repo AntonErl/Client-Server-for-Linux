bf08bd9c95f619482d8d7ffafeca822f38035736
AntonErl <53444077+AntonErl@users.noreply.github.com>
2019-11-14 11:02:35 +0300

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -35,25 +35,25 @@ int main(int argc, const char **argv)
     }
 
     std::string commands = "start";
+    std::getline(std::cin, commands); //Читаем данные из стандарного потока ввода в commands
     while (commands != "exit")
     {
-        std::getline(std::cin, commands);      //Читаем данные из стандарного потока ввода в commands
+
         unsigned mess_len = commands.length(); // размер данных (передаваемого сообщения)
         int bytes_read;
-        if (commands != "exit")
-        {
 
-            char buf[1024];
-            send(sock, commands.c_str(), mess_len + 1, 0);
-            bytes_read=recv(sock, buf, sizeof buf, 0);
-            if (bytes_read <= 0)
-                {
-                    // Соединение разорвано
-                    std::cout << "server close" << std::endl;
-                    break;
-                }
-            std::cout << buf << std::endl;
+        char buf[1024];
+        send(sock, commands.c_str(), mess_len + 1, 0);
+        bytes_read = recv(sock, buf, sizeof buf, 0);
+        if (bytes_read <= 0)
+        {
+            // Соединение разорвано
+            std::cout << "server close" << std::endl;
+            break;
         }
+        std::cout << buf << std::endl;
+
+        std::getline(std::cin, commands); //Читаем данные из стандарного потока ввода в commands
     }
     std::cout << "close client" << std::endl;
     close(sock);
